844ba8b99f8d373f2e761878f6a1296454b6726d
Shawn Cicoria <github@cicoria.com>
2025-06-12 16:38:37 -0400

    using the ? operator

--- scratch/error_handling/src/main.rs
+++ scratch/error_handling/src/main.rs
@@ -1,5 +1,20 @@
 use std::{fs::File, io::Read};
 
+fn read_file_contents(path: &str) -> Result<String, std::io::Error> {
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let file_name = "output.txt";
+    let contents = read_file_contents(file_name)?;
+    println!("File contents: {}", contents);
+    Ok(())
+}
+
+/*
 fn read_file_contents(path: &str) -> String {
     let mut file = File::open(path).unwrap();
     let mut contents = String::new();
@@ -12,3 +27,10 @@ fn main() {
     let contents = read_file_contents(file_name);
     println!("File contents: {}", contents);
 }
+
+
+
+
+
+
+*/
