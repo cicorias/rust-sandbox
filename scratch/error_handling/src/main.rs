1057843a97cf974189bea3ecba751aef0f5e2554
Shawn Cicoria <github@cicoria.com>
2025-06-12 16:29:46 -0400

    init

---
+++ scratch/error_handling/src/main.rs
@@ -0,0 +1,14 @@
+use std::{fs::File, io::Read};
+
+fn read_file_contents(path: &str) -> String {
+    let mut file = File::open(path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    contents
+}
+
+fn main() {
+    let file_name = "output.txt";
+    let contents = read_file_contents(file_name);
+    println!("File contents: {}", contents);
+}
