5b65d56f3d39aec76e5072cbad982aa60bbc0520
Shawn Cicoria <github@cicoria.com>
2025-06-16 16:32:55 -0400

    de-reference *

--- scratch/scratch_board/src/main.rs
+++ scratch/scratch_board/src/main.rs
@@ -1,7 +1,18 @@
 
 
-
-
+/* 
+// dereferencing and references
+fn main() {
+    let mut my_number = 8;
+    let num_ref = &mut my_number;
+    *num_ref += 10;
+    println!("{}", my_number);
+ 
+    let second_number = 800;
+    let triple_reference = &&&second_number;
+    println!("Are they equal? {}", second_number == ***triple_reference);
+}
+*/
 
 /*
 // example display and feature flags
